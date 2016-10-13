15e7437304bfed07041f94d843c2651c4eb7b4de
Logan Byers <lbyers47@gmail.com>
2016-10-13 18:17:57 -0500

    passes all tests chapter 4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -5,17 +5,28 @@ def translate(words)
     translated = strary.collect{|word| 
      if vowel.include?(word[0])
           word << "ay"
-         word
-       elsif !(vowel.include?(word[0])) && !(vowel.include?(word[1]))
+          word
+     elsif word.include?("qu")
+           word.split("qu").to_ary.reverse.join << "qu" << "ay"
+     elsif (!(vowel.include?(word[0])) && !(vowel.include?(word[1])) && !(vowel.include?(word[2]))) 
+          word << word[0] << word[1] << word[2] << "ay"
+          word.slice!(2)
+          word.slice!(1)
+          word.slice!(0)
+          word  
+     
+     elsif (!(vowel.include?(word[0])) && !(vowel.include?(word[1]))) || ((word[0] + word[1]) == "qu")
           word << word[0] << word[1] << "ay"
           word.slice!(1)
           word.slice!(0)
           word
-       else 
+       
+     else 
           word << word[0] << "ay"
           word.slice!(0)
           word
-     end}
+     end
+        }
    translated.join(" ")
 end
 
