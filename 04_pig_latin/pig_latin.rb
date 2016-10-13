9e217f3d23d8b2b56f287d11fb818443db0d251a
Logan Byers <lbyers47@gmail.com>
2016-10-13 17:03:59 -0500

    working mutiple word strings

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
-#write your code here
+def translate(words)
+    vowel = "aeiou" 
+    strary = words.split.to_ary
+    translated = []
+    translated = strary.collect{|word| 
+     if vowel.include?(word[0])
+          word << "ay"
+         word
+       elsif !(vowel.include?(word[0])) && !(vowel.include?(word[1]))
+          word << word[0] << word[1] << "ay"
+          word.slice!(1)
+          word.slice!(0)
+          word
+       else 
+          word << word[0] << "ay"
+          word.slice!(0)
+          word
+     end}
+   translated.join(" ")
+end
+
+    
